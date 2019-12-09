1f41712fc577a174fd8edecfdc125beaf013b843
Leonardvdj <leonardvdj@gmail.com>
2019-12-09 18:19:48 +0100

    Initial commit

---
+++ UI/Components/TOWQuestCounterComponent.cs
@@ -0,0 +1,94 @@
+﻿using LiveSplit.Model;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using TOW.QuestCounter;
+
+namespace LiveSplit.UI.Components
+{
+    class TOWQuestCounterComponent : IComponent
+    {
+        public string ComponentName
+        {
+            get { return "TOW Quest Counter"; }
+        }
+
+        public IDictionary<string, Action> ContextMenuControls { get; protected set; }
+        protected InfoTextComponent InternalComponent;
+
+        private LiveSplitState _state;
+        private string _count;
+
+        Manager outerworlds = new Manager();
+
+        public TOWQuestCounterComponent(LiveSplitState state)
+        {
+            this.InternalComponent = new InfoTextComponent("Quest Count", "0");
+
+            _state = state;
+            _state.OnReset += state_OnReset;
+        }
+
+        public void Dispose()
+        {
+            _state.OnReset -= state_OnReset;
+        }
+
+        public void Update(IInvalidator invalidator, LiveSplitState state, float width, float height, LayoutMode mode)
+        {
+            outerworlds.FindAndHook();
+            outerworlds.UpdateQuests();
+            _count = outerworlds.QuestsCompleted.ToString();
+
+            if (invalidator != null && this.InternalComponent.InformationValue != _count)
+            {
+                this.InternalComponent.InformationValue = _count;
+                invalidator.Invalidate(0f, 0f, width, height);
+            }
+        }
+
+        public void DrawVertical(Graphics g, LiveSplitState state, float width, Region region)
+        {
+            this.PrepareDraw(state);
+            this.InternalComponent.DrawVertical(g, state, width, region);
+        }
+
+        public void DrawHorizontal(Graphics g, LiveSplitState state, float height, Region region)
+        {
+            this.PrepareDraw(state);
+            this.InternalComponent.DrawHorizontal(g, state, height, region);
+        }
+
+        void PrepareDraw(LiveSplitState state)
+        {
+            this.InternalComponent.NameLabel.ForeColor = state.LayoutSettings.TextColor;
+            this.InternalComponent.ValueLabel.ForeColor = state.LayoutSettings.TextColor;
+            this.InternalComponent.NameLabel.HasShadow
+                = this.InternalComponent.ValueLabel.HasShadow
+                = state.LayoutSettings.DropShadows;
+        }
+
+        void state_OnReset(object sender, TimerPhase t)
+        {
+            _count = "";
+        }
+
+        public XmlNode GetSettings(XmlDocument document) { return document.CreateElement("Settings"); }
+        public Control GetSettingsControl(LayoutMode mode) { return null; }
+        public void SetSettings(XmlNode settings) { }
+        public void RenameComparison(string oldName, string newName) { }
+        public float MinimumWidth { get { return this.InternalComponent.MinimumWidth; } }
+        public float MinimumHeight { get { return this.InternalComponent.MinimumHeight; } }
+        public float VerticalHeight { get { return this.InternalComponent.VerticalHeight; } }
+        public float HorizontalWidth { get { return this.InternalComponent.HorizontalWidth; } }
+        public float PaddingLeft { get { return this.InternalComponent.PaddingLeft; } }
+        public float PaddingRight { get { return this.InternalComponent.PaddingRight; } }
+        public float PaddingTop { get { return this.InternalComponent.PaddingTop; } }
+        public float PaddingBottom { get { return this.InternalComponent.PaddingBottom; } }
+    }
+}
---
+++ UI/Components/TOWQuestCounterComponentFactory.cs
@@ -0,0 +1,55 @@
+﻿using LiveSplit.Model;
+using LiveSplit.UI.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: ComponentFactory(typeof(TOWQuestCounterComponentFactory))]
+
+namespace LiveSplit.UI.Components
+{
+    class TOWQuestCounterComponentFactory : IComponentFactory
+    {
+        public string ComponentName
+        {
+            get { return "TOW Quest Counter"; }
+        }
+
+        public string Description
+        {
+            get { return "The Outer Worlds quest completion counter."; }
+        }
+
+        public ComponentCategory Category
+        {
+            get { return ComponentCategory.Information; }
+        }
+
+        public IComponent Create(LiveSplitState state)
+        {
+            return new TOWQuestCounterComponent(state);
+        }
+
+        public string UpdateName
+        {
+            get { return this.ComponentName; }
+        }
+
+        public string UpdateURL
+        {
+            get { return null; }
+        }
+
+        public Version Version
+        {
+            get { return Version.Parse("1.0.0"); }
+        }
+
+        public string XMLURL
+        {
+            get { return null; }
+        }
+    }
+}
